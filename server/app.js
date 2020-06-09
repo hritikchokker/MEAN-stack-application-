93e41f6f2d91eb15dacdc89edbda10805f10c112
hritikchokker <hchokker7@gmail.com>
2020-06-09 22:20:41 +0530

    securing http request packages added

--- server/app.js
+++ server/app.js
@@ -7,13 +7,52 @@ const cors = require('cors');
 const morgan = require('morgan');
 const AppError = require('./utils/appErrors');
 const globalErrorHandler = require('./controllers/errorController');
+const rateLimit = require('exprss-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 
+
+
+// securing http headers
+app.use(helmet());
+
+//logging only in development
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
+
+// creating limit for http headers
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60*60*1000,
+  message: 'Too many request from this IP,please try again after some time'
+})
+
+//limiting http request from same ip
+app.use('/api',limiter);
+
 app.use(express.static(`${__dirname}/public`));
-app.use(express.json());
+app.use(express.json({limit: '10kb'}));
+
+//data sanitization against nosql injection and Xss
+app.use(mongoSanitize());
+app.use(xss());
+
+//prevent parameter pollution
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'ratingQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price'
+  ]
+}));
+
 // app.use(express.urlencoded({ extended: true}));
 app.use(cors());
 
